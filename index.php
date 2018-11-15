c2e50ea15bf65deaee05a599388d1e8e09452953
PhelipeSR <phelipeuni@gmail.com>
2018-11-15 17:43:36 +0000

    resolvendo problema do plataforma cruzada

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
 /**
  *  CodeIgniter
  *
